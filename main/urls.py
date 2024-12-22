e06c2f5bcdf50e9f1f519992462fcbb65bf2c997
Azamat Dosmukhambetov <you@example.com>
2024-12-22 21:36:47 +0500

    new updates

--- main/urls.py
+++ main/urls.py
@@ -15,14 +15,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import MainView
+from .views import MainView, BlogView, AboutView, ContactView
 from django.conf.urls.static import static
 from django.conf import settings
 
+
+
 urlpatterns = [
-    path("", MainView.as_view(), name="main"),
+    path("", MainView.as_view(),name='home'),
+    path('blog/', BlogView.as_view(), name = 'blog'),
+    path('about/', BlogView.as_view(), name = 'about'),
+    path('contact/', BlogView.as_view(), name = 'contact')
 ]
+
 urlpatterns += static(
     settings.STATIC_URL,
     document_root=settings.STATICFILES_DIRS
 )
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
--- main/views.py
+++ main/views.py
@@ -4,3 +4,12 @@ from django.views.generic import TemplateView
 
 class MainView(TemplateView):
     template_name = "index.html"
+    
+class BlogView(TemplateView):
+    template_name = "blog.html"
+    
+class AboutView(TemplateView):
+    template_name = "about.html"
+    
+class ContactView(TemplateView):
+    template_name = "contact.html"
